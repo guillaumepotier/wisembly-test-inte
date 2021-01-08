72416038f426cfe58ae39a7ada14b2edf92b4755
nicolasmty <nicolas.mnty@gmail.com>
2021-01-08 18:06:35 +0100

    Header

---
+++ reproduction/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    $('#anonyme-switch').click(function() {
+         $(this).toggleClass('visible');
+    });
+    
+    $('#menu-hamb').click(function() {
+       $(this).toggleClass('active'); 
+    });
+    
+});
