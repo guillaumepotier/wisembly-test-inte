86bfaa52be9467b628aa6b188a1643cc93e0a866
nicolasmty <nicolas.mnty@gmail.com>
2021-01-08 18:46:31 +0100

    Content

--- reproduction/script.js
+++ reproduction/script.js
@@ -8,4 +8,8 @@ $(document).ready(function() {
        $(this).toggleClass('active'); 
     });
     
+    $('#img-fullscreen').click(function() {
+       $('#lightbox').addClass('active'); 
+    });
+    
 });
