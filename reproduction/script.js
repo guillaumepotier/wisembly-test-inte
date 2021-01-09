511e4102b65d5c2da8e274198305135354acc714
nicolasmty <nicolas.mnty@gmail.com>
2021-01-09 14:45:42 +0100

    Opinion selection feedback

--- reproduction/script.js
+++ reproduction/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     
+    // Header
+    
     $('#anonyme-switch').click(function() {
          $(this).toggleClass('visible');
     });
@@ -8,6 +10,8 @@ $(document).ready(function() {
        $(this).toggleClass('active'); 
     });
     
+    // Content
+    
     $('#img-fullscreen').click(function() {
        $('#lightbox').addClass('active'); 
     });
@@ -15,6 +19,18 @@ $(document).ready(function() {
     $('#opinion-button').click(function() {
        $('#opinion-content').toggleClass('open'); 
     });
+    
+    $('.opinion-smiley').click(function() {
+        $('.opinion-smiley').removeClass('selected');
+        $(this).addClass('selected');
+        var opinionUrl = $(this).css('background-image');
+        $('#opinion-button').css('background-image', opinionUrl);
+        setTimeout(function(){
+            $('#opinion-content').toggleClass('open');
+        }, 500);
+    });
+    
+    // Tab bar
     
     $('.tab-element').click(function() {
         $('.tab-element').removeClass('selected');
